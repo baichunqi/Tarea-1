e92599ebdf538a3775fa7256e57e36b9b0fe5ce2
baichunqi <baichun.qi@elorrieta-errekamari.com>
2023-09-13 21:26:45 +0200

    Cambios

--- Pruebas/app/src/main/java/com/example/pruebas/MainActivity.kt
+++ Pruebas/app/src/main/java/com/example/pruebas/MainActivity.kt
@@ -11,12 +11,59 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val boton: Button = findViewById<Button>(R.id.button17)
-        val texto:TextView = findViewById<TextView>(R.id.textView10)
+        val boton1: Button = findViewById<Button>(R.id.button1)
+        val boton2: Button = findViewById<Button>(R.id.button2)
+        val boton3: Button = findViewById<Button>(R.id.button3)
+        val boton4: Button = findViewById<Button>(R.id.button4)
+        val boton5: Button = findViewById<Button>(R.id.button5)
+        val boton6: Button = findViewById<Button>(R.id.button6)
+        val boton7: Button = findViewById<Button>(R.id.button7)
+        val boton8: Button = findViewById<Button>(R.id.button8)
+        val boton9: Button = findViewById<Button>(R.id.button9)
+        val boton0: Button = findViewById<Button>(R.id.button0)
 
-        boton.setOnClickListener{
-            contador += 1
-            texto.text = contador.toString()
+        val texto:TextView = findViewById<TextView>(R.id.Numero)
+
+        boton1.setOnClickListener{
+
+            texto.text = 1.toString()
         }
+        boton2.setOnClickListener{
+
+            texto.text = 2.toString()
+        }
+        boton3.setOnClickListener{
+
+            texto.text = 3.toString()
+        }
+        boton4.setOnClickListener{
+
+            texto.text = 4.toString()
+        }
+        boton5.setOnClickListener{
+
+            texto.text = 5.toString()
+        }
+        boton6.setOnClickListener{
+
+            texto.text = 6.toString()
+        }
+        boton7.setOnClickListener{
+
+            texto.text = 7.toString()
+        }
+        boton8.setOnClickListener{
+
+            texto.text = 8.toString()
+        }
+        boton9.setOnClickListener{
+
+            texto.text = 9.toString()
+        }
+        boton0.setOnClickListener{
+
+            texto.text = 0.toString()
+        }
+
     }
 }
